2a0b90c665f007b408a08609d44c5a07a11a10b1
wvdlaan <wvdlaan@canvasheroes.com>
2025-07-10 10:03:03 +0300

    fix: store hooks specified to slice

--- src/hooks/store-hooks/use-auth-store.ts
+++ src/hooks/store-hooks/use-auth-store.ts
@@ -2,9 +2,17 @@
 
 import { useShallow } from "zustand/shallow";
 import { useStore } from "./use-store";
+import { AuthSlice } from "@/store/slices/auth-slice";
 
 const useAuthStore = () => {
-    const authStore = useStore(useShallow((state) => state));
+    const authStore: AuthSlice = useStore(
+        useShallow((state) => ({
+            token: state.token,
+            isAuthenticated: state.isAuthenticated,
+            setToken: state.setToken,
+            clearAuth: state.clearAuth,
+        })),
+    );
     if (!authStore) throw Error("useAuthStore used outside provider");
 
     return authStore;
--- src/hooks/store-hooks/use-canvas-store.ts
+++ src/hooks/store-hooks/use-canvas-store.ts
@@ -2,12 +2,41 @@
 
 import { useShallow } from "zustand/shallow";
 import { useStore } from "./use-store";
+import { CanvasSlice } from "@/store/slices/canvas-slice";
 
 const useCanvasStore = () => {
-    const canvasStore = useStore(useShallow((state) => state));
+    const canvasStore: CanvasSlice = useStore(
+        useShallow((state) => ({
+            type: state.type,
+            color: state.color,
+            colorHistory: state.colorHistory,
+            opacity: state.opacity,
+            size: state.size,
+            layers: state.layers,
+            activeLayerIndex: state.activeLayerIndex,
+            redoStack: state.redoStack,
+            scale: state.scale,
+            position: state.position,
+            zoomType: state.zoomType,
+            setColor: state.setColor,
+            addColorToHistory: state.addColorToHistory,
+            setSize: state.setSize,
+            setOpacity: state.setOpacity,
+            setType: state.setType,
+            resetBrush: state.resetBrush,
+            setLayers: state.setLayers,
+            switchActiveLayer: state.switchActiveLayer,
+            addLayer: state.addLayer,
+            removeLayer: state.removeLayer,
+            resetLayers: state.resetLayers,
+            resetHistory: state.resetHistory,
+            setZoomType: state.setZoomType,
+            setScale: state.setScale,
+            setPosition: state.setPosition,
+        })),
+    );
 
     if (!canvasStore) throw Error("useCanvasStore used outside provider");
-
     return canvasStore;
 };
 
