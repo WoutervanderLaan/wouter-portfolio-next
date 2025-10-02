dde9c0c1d1b63ad621765d676be9308696a7754e
Wouter van der Laan <wouter_vanderlaan93@hotmail.com>
2025-10-02 15:00:20 +0200

    wrong env var

--- src/app/api/trello/webhook/route.ts
+++ src/app/api/trello/webhook/route.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-const TRELLO_TOKEN = process.env.TRELLO_TOKEN!;
+const TRELLO_SECRET = process.env.TRELLO_SECRET!;
 const TRELLO_CALLBACK_URL = process.env.TRELLO_CALLBACK_URL!;
 
 export async function GET() {
@@ -20,18 +20,18 @@ export async function POST(req: Request) {
         return new Response("Missing Trello signature", { status: 400 });
     }
 
-    // const isValid = verifyTrelloSignature(
-    //     rawBody,
-    //     TRELLO_CALLBACK_URL,
-    //     signature ?? "",
-    //     TRELLO_TOKEN,
-    // );
-
-    const isValid = verifyTrelloWebhookRequest(
-        req,
-        TRELLO_TOKEN,
+    const isValid = verifyTrelloSignature(
+        rawBody,
         TRELLO_CALLBACK_URL,
+        signature ?? "",
+        TRELLO_SECRET,
     );
+
+    // const isValid = verifyTrelloWebhookRequest(
+    //     req,
+    //     TRELLO_SECRET,
+    //     TRELLO_CALLBACK_URL,
+    // );
 
     if (!isValid) {
         return new Response("Invalid Trello signature", { status: 403 });
@@ -57,43 +57,43 @@ export async function POST(req: Request) {
     });
 }
 
-// function verifyTrelloSignature(
-//     rawBody: string,
-//     callbackUrl: string,
-//     receivedSig: string,
-//     secret: string,
-// ) {
-//     const hmac = crypto.createHmac("sha1", secret);
-
-//     hmac.update(rawBody + callbackUrl, "utf8");
-
-//     const expected = hmac.digest("base64");
-
-//     const expectedBuf = Buffer.from(expected, "base64");
-//     const receivedBuf = Buffer.from(receivedSig, "base64");
-
-//     if (expectedBuf.length !== receivedBuf.length) {
-//         console.warn("Signature length mismatch");
-//         return false;
-//     }
-//     console.log("expected", expected);
-//     console.log("received", receivedSig);
-//     return crypto.timingSafeEqual(expectedBuf, receivedBuf);
-// }
-
-function verifyTrelloWebhookRequest(
-    request: Request,
+function verifyTrelloSignature(
+    rawBody: string,
+    callbackUrl: string,
+    receivedSig: string,
     secret: string,
-    callbackURL: string,
 ) {
-    const base64Digest = function (s: crypto.BinaryLike) {
-        return crypto.createHmac("sha1", secret).update(s).digest("base64");
-    };
+    const hmac = crypto.createHmac("sha1", secret);
 
-    const content = JSON.stringify(request.body) + callbackURL;
-    const expected = base64Digest(content);
-    const received = request.headers.get("x-trello-webhook");
-    console.log("expected2", expected);
-    console.log("received2", received);
-    return expected === received;
+    hmac.update(rawBody + callbackUrl, "utf8");
+
+    const expected = hmac.digest("base64");
+
+    const expectedBuf = Buffer.from(expected, "base64");
+    const receivedBuf = Buffer.from(receivedSig, "base64");
+
+    if (expectedBuf.length !== receivedBuf.length) {
+        console.warn("Signature length mismatch");
+        return false;
+    }
+    console.log("expected3", expected);
+    console.log("received3", receivedSig);
+    return crypto.timingSafeEqual(expectedBuf, receivedBuf);
 }
+
+// function verifyTrelloWebhookRequest(
+//     request: Request,
+//     secret: string,
+//     callbackURL: string,
+// ) {
+//     const base64Digest = function (s: crypto.BinaryLike) {
+//         return crypto.createHmac("sha1", secret).update(s).digest("base64");
+//     };
+
+//     const content = JSON.stringify(request.body) + callbackURL;
+//     const expected = base64Digest(content);
+//     const received = request.headers.get("x-trello-webhook");
+//     console.log("expected2", expected);
+//     console.log("received2", received);
+//     return expected === received;
+// }
